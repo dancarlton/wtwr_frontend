19d3ccdaa6d23d2121f3a21aaa10a0d7f01ee2d6
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-18 14:39:00 -0700

    changing to base /

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/wtwr_frontend/' : '/',
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
-    port: 3000,
-  },
-}))
+    port: 3000
+  }
+})
